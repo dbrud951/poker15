dab5e301e79b9f75411abea1e6565df70c1dd00b
Yang Daeun <wt3030@hanmail.net>
2017-04-26 15:38:52 +0900

    테스트순서변경

--- src/test/java/com/edu/cnu/poker/EvaluatorTest.java
+++ src/test/java/com/edu/cnu/poker/EvaluatorTest.java
@@ -42,6 +42,20 @@ public class EvaluatorTest {
     }
 
     @Test
+    public void Rank_2개가_동일한게_두쌍이면_투페어다() {
+        Evaluator evaluator = new Evaluator();
+        List<Card> cardList = Arrays.asList(
+                new Card(4,Suit.CLUBS),
+                new Card(4,Suit.DIAMONDS),
+                new Card(13,Suit.SPADES),
+                new Card(13,Suit.HEARTS),
+                new Card(2,Suit.CLUBS)
+        );
+        Ranking result = evaluator.evaluate(cardList);
+        assertThat(result, is(Ranking.TwoPairs));
+    }
+
+    @Test
     public void Rank_2개가_동일한게_한쌍이면_원페어다() {
         Evaluator evaluator = new Evaluator();
         List<Card> cardList = Arrays.asList(
@@ -55,19 +69,7 @@ public class EvaluatorTest {
         assertThat(result, is(Ranking.OnePair));
     }
 
-    @Test
-    public void Rank_2개가_동일한게_두쌍이면_투페어다() {
-        Evaluator evaluator = new Evaluator();
-        List<Card> cardList = Arrays.asList(
-                new Card(4,Suit.CLUBS),
-                new Card(4,Suit.DIAMONDS),
-                new Card(13,Suit.SPADES),
-                new Card(13,Suit.HEARTS),
-                new Card(2,Suit.CLUBS)
-        );
-        Ranking result = evaluator.evaluate(cardList);
-        assertThat(result, is(Ranking.TwoPairs));
-    }
+
 
 
 }
