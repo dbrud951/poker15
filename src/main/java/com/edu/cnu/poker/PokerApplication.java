2ea60fcb98d84f6e916c6267720c9038046420a6
SuJoung Kim <abbey17@naver.com>
2017-04-28 14:09:57 +0900

    PokerApplication 수정

--- src/main/java/com/edu/cnu/poker/PokerApplication.java
+++ src/main/java/com/edu/cnu/poker/PokerApplication.java
@@ -1,5 +1,10 @@
 package com.edu.cnu.poker;
 
+import java.util.Scanner;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.isA;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Created by cse on 2017-04-17.
  * CARD - rank, suit
@@ -12,6 +17,152 @@ package com.edu.cnu.poker;
 public class PokerApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello Poker");
+        int money = 100;
+        Scanner sc = new Scanner(System.in);
+        int bet;
+        int whoIsWin;
+
+
+        System.out.println("Welcome to the game of Poker.");
+
+        while(true){
+            System.out.println("You have "+money+" dollars.");
+            System.out.println("How many dollars do you want to bet?  (Enter 0 to end.)");
+            do{
+                bet = sc.nextInt();
+                if(bet<0 || bet> money){
+                    System.out.println("Your answer must be between 0 and " + money + ".");
+                }
+            }while(bet<0 || bet > money);
+            if (bet == 0){
+                break;
+            }
+            whoIsWin = playPoker();
+            if(whoIsWin == 1){
+                System.out.println("You got Money!");
+                money = money + bet;
+            }else if(whoIsWin == 2){
+                System.out.println("You lose Money. Thanks!");
+                money = money - bet;
+            }else if(whoIsWin == 3){
+                System.out.println("You push with Dealer.");
+            }else{
+                System.out.println("Die.");
+            }
+            if(money == 0){
+                System.out.println("Looks like you've are out of money!");
+                break;
+            }
+        }
+
+        System.out.println("You leave with "+money+".");
+        System.out.println("Good Bye!");
+
+
+    }//main의 끝
+
+    static int playPoker(){
+
+        Deck deck = new Deck(1);
+        Hand userHand = new Hand(deck, PokerType.FIVE);
+        Hand dealerHand = new Hand(deck, PokerType.FIVE);
+        Evaluator evaluator = new Evaluator();
+        String userResult;
+        String dealerResult;
+
+        int userPoint;
+        int dealerPoint;
+
+        userResult = evaluator.evaluate(userHand.getCardList()).toString();
+        dealerResult = evaluator.evaluate(dealerHand.getCardList()).toString();
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            if(userHand.getTotalCard() == 0){
+                System.out.println("No more Cards... Can't Open.");
+            }else{
+                System.out.print("Your cards are : ");
+                Card userCard = userHand.getCard();
+                System.out.println(userCard.getSuit() + " " + userCard.getRank() + " ");
+                System.out.print("Dealer is showing the : ");
+                Card dealerCard = dealerHand.getCard();
+                System.out.println(dealerCard.getSuit() + " " + dealerCard.getRank() + " ");
+            }
+            System.out.println(" O(Open) / G(Go) / D(Die) ? ");
+            char a;
+            do{
+                a = sc.nextLine().charAt(0);
+                a = Character.toUpperCase(a);
+                if(a != 'O' && a != 'G' && a != 'D'){
+                    System.out.println("Please respond O or G or D");
+                }
+            }while(a != 'O' && a != 'G' && a != 'D');
+
+            if(a == 'O'){//open
+                System.out.println("Another Card is Open...");
+            }else if(a == 'G'){//go
+
+                userPoint = rankPoker(userResult);
+                dealerPoint = rankPoker(dealerResult);
+                System.out.println(" You : "+userResult + "\n Dealer : "+dealerResult);
+                if(userPoint > dealerPoint){
+                    System.out.println("You win!");
+                    return 1;
+                }else if(userPoint < dealerPoint){
+                    System.out.println("You Lose!");
+                    return 2;
+                }else{
+//                    System.out.println("Push.");
+                    return 3;
+                }
+            }else{//die
+                return 4;
+            }
+
+
+        }
+
+    }//playPoker의 끝
+
+    static int rankPoker(String ranking){
+        if(ranking.equals("OnePair")){
+            return 1;
+        }else if(ranking.equals("TwoPairs")){
+            return 2;
+        }else if(ranking.equals("ThreeOfaKind")){
+            return 3;
+        }else if(ranking.equals("Straight")){
+            return 4;
+        }
+        else if(ranking.equals("BackStraight")){
+            return 5;
+        }
+        else if(ranking.equals("Mountian")){
+            return 6;
+        }
+        else if(ranking.equals("Flush")){
+            return 7;
+        }
+        else if(ranking.equals("FullHouse")){
+            return 8;
+        }else if(ranking.equals("FourOfaKind")){
+            return 9;
+        }else if(ranking.equals("StraightFlush")){
+            return 10;
+        }
+        else if(ranking.equals("BackStraightFlush")){
+            return 11;
+        }
+        else if(ranking.equals("RoyalStraightFlush")){
+            return 12;
+        }else{
+            return 0;
+        }
+
+//        assertThat(ranking,is(Ranking.OnePair));
+
     }
+
+
 }
